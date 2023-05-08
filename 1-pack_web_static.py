3c282c066e00aff5cbfbb20c59f99d74fc294cd9
Ayandah Dube <ayandah.dube@liquid.tech>
2023-05-08 16:00:43 +0200

    fix 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Fabfile that create a .tgz archive from
-the contents of web_static folder"""
-
-# if __name__ == '__main__':
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack
+"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """Pack all the contents in the web_static directory
-    as a tar archive"""
-
+    """
+    create the archive file with the contents of the web_static folder
+    and return the archive path if the archive has been correctly generated
+    otherwise return None
+    """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(datetime_str)
     try:
-        local("mkdir -p versions")
-        time = datetime.now()
-        date_string = '%Y%m%d%H%M%S'
-        date = time.strftime(date_string)
-
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -czvf {} web_static".format(file_path))
-        return file_path
-
-    except Exception:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(file_name))
+        return "versions/{}".format(file_name)
+    except:
         return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
-""""Fabric script that distributes an archive to web servers"""
-
-from fabric.api import *
+"""
+Fabric script that distributes an archive to your web servers
+using the function do_deploy
+"""
+from fabric.api import env, run, put, hosts
+# from datetime import datetime
 import os
 
-env.hosts = ['3.235.198.120', '3.239.50.204']
+env.hosts = ['34.74.23.57', '35.196.161.89']
 
 
 def do_deploy(archive_path):
-    """Archive distributor"""
+    """
+    upload the archive to the /tmp/ directory of the web server
+    uncompress the archive
+    delete the archive from the web server
+    handle symbolic links
+    return True is all operations went well, False otherwise
+    """
+    if not os.path.exists(archive_path):
+        return False
     try:
-        try:
-            if os.path.exists(archive_path):
-                arc_tgz = archive_path.split("/")
-                arg_save = arc_tgz[1]
-                arc_tgz = arc_tgz[1].split('.')
-                arc_tgz = arc_tgz[0]
-
-                """Upload archive to the server"""
-                put(archive_path, '/tmp')
-
-                """Save folder paths in variables"""
-                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
-                tmp_location = '/tmp/{}'.format(arg_save)
-
-                """Run remote commands on the server"""
-                run('mkdir -p {}'.format(uncomp_fold))
-                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
-                run('rm {}'.format(tmp_location))
-                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
-                run('rm -rf {}/web_static'.format(uncomp_fold))
-                run('rm -rf /data/web_static/current')
-                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
-                run('sudo service nginx restart')
-                return True
-            else:
-                print('File does not exist')
-                return False
-        except Exception as err:
-            print(err)
-            return False
-    except Exception:
-        print('Error')
+        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[-1]
+        file_name_noext = file_name.split('.')[0]
+        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
+        run('sudo mkdir -p {}'.format(new_folder))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
+        run('sudo rm -rf {}web_static'.format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
+        print("New version deployed!")
+        return True
+    except:
         return False
